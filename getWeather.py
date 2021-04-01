01e8fe9572f5657f069d182346e45c4c11d92abb
Kevin C. Escobedo <escobedo001@gmail.com>
2021-04-01 10:47:58 -0700

    Added function to get weather data

---
+++ getWeather.py
@@ -0,0 +1,29 @@
+import secret #secret.py contains the OpenWeathermap API key
+import requests
+import json
+
+def weatherData(city: str) -> tuple:
+    '''
+    Calls the OpenWeathermap API
+    Returns the temperature in Kelvin,
+    the pressure in hPa,
+    the humidity percentage,
+    and the weather conditions of the
+    specified city.
+    '''
+    baseURL = "http://api.openweathermap.org/data/2.5/weather?"
+    completeURL = "{}appid={}&q={}".format(baseURL, secret.APIKEY, city)
+    response = requests.get(completeURL)
+    x = response.json()
+    
+    if x["cod"] != "404":
+        y = x["main"]
+        currentTemperature = y["temp"]
+        currentPressure = y["pressure"]
+        currentHumidity = y["humidity"]
+        z = x["weather"]
+        weatherConditions = z[0]["description"]
+
+        return (currentTemperature, currentPressure, currentHumidity, weatherConditions)
+    else:
+        return None
